0af7c4a27737d65b2f56915cd7b42604335c2c39
Johan Alpne <johan.alpne@funnel.io>
2022-12-02 15:05:16 +0100

    day 2: algorithm for determining winner

--- days/day2.py
+++ days/day2.py
@@ -1,11 +1,20 @@
 from helper.utils import *
+from enum import Enum
 
 
 DAY = 2
+
+
+class Sign(Enum):
+    ROCK = 0
+    PAPER = -1
+    SCISSORS = 1
+
+
 opp_map = {
-    "A": "rock",
-    "B": "paper",
-    "C": "scissor"
+    "A": Sign.ROCK,
+    "B": Sign.PAPER,
+    "C": Sign.SCISSORS
 }
 
 
@@ -13,7 +22,7 @@ opp_map = {
 def prepare_data():
     raw_data = parse_file_rows_to_list(DAY)
     rounds = [
-        (round[0], round[2])
+        (opp_map[round[0]], round[2])
         for round in raw_data
     ]
     return rounds
@@ -22,54 +31,76 @@ def prepare_data():
 @time_function
 def part_a(data):
     me_map = {
-        "X": "rock",
-        "Y": "paper",
-        "Z": "scissor"
+        "X": Sign.ROCK,
+        "Y": Sign.PAPER,
+        "Z": Sign.SCISSORS
     }
-    return sum([get_score(opp_map[opp], me_map[me]) for opp, me in data])
+    return sum([get_score(opp, me_map[me]) for opp, me in data])
 
 
 @time_function
 def part_b(data):
     strategies = {
-        "rock": {
-            "X": "scissor",
-            "Y": "rock",
-            "Z": "paper"
+        Sign.ROCK: {
+            "X": Sign.SCISSORS,
+            "Y": Sign.ROCK,
+            "Z": Sign.PAPER
         },
-        "paper": {
-            "X": "rock",
-            "Y": "paper",
-            "Z": "scissor"
+        Sign.PAPER: {
+            "X": Sign.ROCK,
+            "Y": Sign.PAPER,
+            "Z": Sign.SCISSORS
         },
-        "scissor": {
-            "X": "paper",
-            "Y": "scissor",
-            "Z": "rock"
+        Sign.SCISSORS: {
+            "X": Sign.PAPER,
+            "Y": Sign.SCISSORS,
+            "Z": Sign.ROCK
         }
     }
-    return sum([get_score(opp_map[opp], strategies[opp_map[opp]][strategy]) for opp, strategy in data])
+    return sum([get_score_new(opp, strategies[opp][strategy]) for opp, strategy in data])
 
 
 def get_score(opp, me):
-    ch_score = {"rock": 1, "paper": 2, "scissor": 3}
+    ch_score = {Sign.ROCK: 1, Sign.PAPER: 2, Sign.SCISSORS: 3}
     score = ch_score[me]
     match (opp, me):
         case _ if opp == me:
             score += 3
-        case ("rock", "paper"):
+        case (Sign.ROCK, Sign.PAPER):
             score += 6
-        case ("rock", "scissor"):
+        case (Sign.ROCK, Sign.SCISSORS):
             score += 0
-        case ("paper", "rock"):
+        case (Sign.PAPER, Sign.ROCK):
             score += 0
-        case ("paper", "scissor"):
+        case (Sign.PAPER, Sign.SCISSORS):
             score += 6
-        case ("scissor", "rock"):
+        case (Sign.SCISSORS, Sign.ROCK):
             score += 6
-        case ("scissor", "paper"):
+        case (Sign.SCISSORS, Sign.PAPER):
             score += 0
     return score
+
+
+def get_score_new(opp, me):
+    ch_score = {Sign.ROCK: 1, Sign.PAPER: 2, Sign.SCISSORS: 3}
+    score = ch_score[me]
+    result = _get_result(opp, me)
+    match result:
+        case 2:
+            score += 3
+        case _ if result == me.value:
+            score += 6
+    return score
+
+
+def _get_result(hand1, hand2):
+    winning_hand = 2  # draw
+    if hand1 != hand2:
+        if abs(hand1.value) == abs(hand2.value):  # paper and scissors
+            winning_hand = max(hand1.value, hand2.value)
+        else:
+            winning_hand = min(hand1.value, hand2.value)
+    return winning_hand
 
 
 def main():
