835730d9e91f7a397f429d95913a1b8be5af8725
Johan Alpne <johan.alpne@gmail.com>
2022-12-05 23:36:37 +0100

    day5 cleanup

--- days/day5.py
+++ days/day5.py
@@ -21,12 +21,12 @@ def prepare_data():
     for row in reversed(rows):
         for i, col in enumerate(row):
             if len(col) == 1:
-                columns[i].append(col)
+                columns[i + 1].append(col)
 
     moves = []
     for row in raw_data:
         if row.startswith("move"):
-            moves.append([int(m) for m in pattern_moves.findall(row)])
+            moves.append([int(n) for n in pattern_moves.findall(row)])
 
     return columns, moves
 
@@ -34,35 +34,35 @@ def prepare_data():
 @time_function
 def part_a(data):
     columns, moves = data
-    for move in moves:
-        qty, frm, to = move
-        frm -= 1
-        to -= 1
-        crates_moving = reversed(columns[frm][-qty:])
-        columns[to] += crates_moving
-        columns[frm] = columns[frm][:-qty]
-    msg = "".join([
-        col[-1]
-        for _, col in columns.items()
-    ])
-    return msg
+    _execute_moves(columns, moves)
+    return _get_top_crates(columns)
 
 
 @time_function
 def part_b(data):
     columns, moves = data
+    _execute_moves(columns, moves, move_all=True)
+    return _get_top_crates(columns)
+
+
+def _execute_moves(columns, moves, move_all=False):
     for move in moves:
         qty, frm, to = move
-        frm -= 1
-        to -= 1
-        crates_moving = columns[frm][-qty:]
+
+        if move_all:
+            crates_moving = columns[frm][-qty:]
+        else:
+            crates_moving = reversed(columns[frm][-qty:])
+
         columns[to] += crates_moving
         columns[frm] = columns[frm][:-qty]
-    msg = "".join([
+
+
+def _get_top_crates(arrangement):
+    return "".join([
         col[-1]
-        for _, col in columns.items()
+        for _, col in arrangement.items()
     ])
-    return msg
 
 
 def main():
